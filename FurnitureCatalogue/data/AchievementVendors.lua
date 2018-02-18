6c0a7af9ae3ad20cb568457e3960e9f61ed4a66c
git <vivec@telvanni.eu>
2018-02-18 18:04:48 +0100

    added two new items to undaunted quartermaster, luxury vendor

--- FurnitureCatalogue/data/AchievementVendors.lua
+++ FurnitureCatalogue/data/AchievementVendors.lua
@@ -1,3 +1,19 @@
+FurC.AchievementVendors[FURC_DRAGONS] = {
+	["the Undaunted Enclaves"] = {
+		["Undaunted Quartermaster"] = {
+			[134908] = { -- Blackmarrow Banner
+				itemPrice 	= 15000, 
+				achievement = 1975, -- Scalecaller Peak Vanquisher 
+			},
+			[134907] = { 
+				itemPrice 	= 50000, 
+				achievement = 1959, -- Fang Lair Vanquisher 
+			},
+		},
+	},
+}
+
+
 FurC.AchievementVendors[FURC_CLOCKWORK] = {
 
 
