ce4ed6a0155730f5310f5945e43ff636b6c9e3da
git <vivec@telvanni.eu>
2018-05-20 11:16:40 +0200

    2.4.1 updated LibAsync

--- startup.lua
+++ startup.lua
@@ -1,7 +1,7 @@
 FurnitureCatalogue 					= {}
 FurnitureCatalogue.name				= "FurnitureCatalogue"
 FurnitureCatalogue.author			= "manavortex"
-FurnitureCatalogue.version          = "2.4"
+FurnitureCatalogue.version          = "2.4.1"
 FurnitureCatalogue.CharacterName	= nil
 FurnitureCatalogue.settings			= {}
 
