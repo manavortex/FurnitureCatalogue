a12f93cf05c7c6ff9c819a9c2b59326bf5f6a0be
git <vivec@telvanni.eu>
2018-07-27 12:30:30 +0200

    :|

--- data/02_Morrowind/M_LuxuryFurnisher.lua
+++ data/02_Morrowind/M_LuxuryFurnisher.lua
@@ -2,23 +2,6 @@ FurC.LuxuryFurnisher = FurC.LuxuryFurnisher or {}
 
 FurC.LuxuryFurnisher[FURC_MORROWIND] = {
 
-	-- August 5+6
-	[126573] = { -- Velothi Candle, Mourning
-		itemPrice 	= 5417,
-		itemDate	= "2017-08-11",
-	},
-	[126574] = { -- Velothi Cerecloth, Prayer
-		itemPrice 	= 5417,
-		itemDate	= "2017-08-11",
-	},
-	[126575] = { -- Velothi Seat, Meditation
-		itemPrice 	= 10834,
-		itemDate	= "2017-08-11",
-	},
-	[126572] = { -- Velothi Shroud, Mourning
-		itemPrice 	= 5417,
-		itemDate	= "2017-08-11",
-	},
 
 
 
