25acada1a7a800c1da44dca77488e316fd636e45
git <vivec@telvanni.eu>
2018-09-08 12:46:26 +0200

    luxury furnisher

--- startup.lua
+++ startup.lua
@@ -1,7 +1,7 @@
 FurnitureCatalogue 					= {}
 FurnitureCatalogue.name				= "FurnitureCatalogue"
 FurnitureCatalogue.author			= "manavortex"
-FurnitureCatalogue.version          = 2.63
+FurnitureCatalogue.version          = 2.64
 FurnitureCatalogue.CharacterName	= nil
 FurnitureCatalogue.settings			= {}
 
