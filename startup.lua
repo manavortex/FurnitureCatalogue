b4564508d46ac89af8ad9f69395afe3782b8d40f
git <vivec@telvanni.eu>
2019-06-22 14:24:39 +0200

    3.52

--- startup.lua
+++ startup.lua
@@ -1,7 +1,7 @@
 FurnitureCatalogue                = {}
 FurnitureCatalogue.name           = "FurnitureCatalogue"
 FurnitureCatalogue.author         = "manavortex"
-FurnitureCatalogue.version        = 3.51
+FurnitureCatalogue.version        = 3.52
 FurnitureCatalogue.CharacterName  = nil
 FurnitureCatalogue.settings       = {}
 
