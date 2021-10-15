6b035421274a3f8d1d3cf7d31755a5ef2464cb16
manavortex <manavortex@gmail.com>
2021-10-15 21:28:23 +0200

    Update ApiVersion.lua

--- data/ApiVersion.lua
+++ data/ApiVersion.lua
@@ -12,8 +12,8 @@
 local tblIndex = FURC_KITTY
 
 FurC.Recipes[tblIndex] = {
-  152064,  Elsweyr Table, Low Square,
-  152065,  Elsweyr Desk, Elegant Wooden,
+  152064,  -- Elsweyr Table, Low Square,
+  152065,  -- Elsweyr Desk, Elegant Wooden,
 }
 
 FurC.MiscItemSources[tblIndex]  = {
