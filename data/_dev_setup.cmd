15d1953b6eb994b4adadb25223306a9f67247098
manavortex <manavortex@gmail.com>
2021-11-27 15:29:45 +0100

    fixed a comma, added helper file

---
+++ data/_dev_setup.cmd
@@ -0,0 +1,8 @@
+@echo off
+setlocal enabledelayedexpansion 
+
+if not exist "..\..\sidTools" (
+	echo Please install the AddOn "sidTools" from esoui: https://www.esoui.com/downloads/info1210-sidTools.html
+)
+
+endlocal
