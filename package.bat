3662fa81681ed4f7fc9d1ca1491d066b3ada985d
manavortex <manavortex@gmail.com>
2021-11-28 21:00:38 +0100

    4.21115

--- package.bat
+++ package.bat
@@ -11,85 +11,85 @@ REM SET ESOUI_URL=""
 SET ESOUI_URL="https://www.esoui.com/downloads/editfile.php?id=1617"
 
 :: Set target directory to anything but "" to have the script move the generated zip file there
-:: !USERPROFILE! will be resolved to "C:\Users\<yourusername>
+:: %USERPROFILE% will be resolved to "C:\Users\<yourusername>
 REM SET TARGET_DIRECTORY=""
-SET TARGET_DIRECTORY="!USERPROFILE!\Dropbox"
+SET TARGET_DIRECTORY="%USERPROFILE%\Dropbox"
 
 :: Set github branch to anything but "" to have this script automatically push to github 
 :: SET GITHUB_BRANCH=""
 SET GITHUB_BRANCH="master"
 
-set EXCLUDE_FROM_PACK=Custom.lua^
-	_dev_setup.cmd
-
+SET DELETE_CUSTOM_FILENAME="Custom.lua"
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 :: check for existence of 7zip
 set zip="%ProgramFiles%\7-Zip\7z.exe"
-if not exist !zip! goto :zipnotfound
+if not exist %zip% goto :zipnotfound
 
 :: read addon name from manifest.txt
 for %%* in (.) do set name=%%~nx*
 
-if not exist !name!.txt (
+if not exist %name%.txt (
   echo * Please enter the name of your add-on:
   set /P name=^>
 )
 
 :: read addon version from txt
-for /F "tokens=3" %%i in ('findstr /C:"## Version:" !name!.txt') do set version=%%i
+for /F "tokens=3" %%i in ('findstr /C:"## Version:" %name%.txt') do set version=%%i
 
 
 :: throw it on git
-IF NOT !GITHUB_BRANCH! == "" (
-	echo * Pushing to github branch !GITHUB_BRANCH! with commit message !VERSION!...
-	git commit -am "!VERSION!"	
+IF NOT %GITHUB_BRANCH% == "" (
+	echo * Pushing to github branch %GITHUB_BRANCH% with commit message %version%...
+	git commit -am "%version%"
+	:: Strip ""s 
 	git push origin %GITHUB_BRANCH:"=%
 )
 
-set archive=!name!-!version!.zip
+set archive=%name%-%version%.zip
 
-echo * Packaging !archive!...
-md .package\!name!
+echo * Packaging %archive%...
+md .package\%name%
 
 :: read files from manifest
-set files=!name!.txt
+set files=%name%.txt
 for /F %%i in ('findstr /B /R "[^#;]" %name%.txt') do (
   set file=%%~nxi
-  set files=%files% %file:$^(language^)=*%
+  set files=!files! !file:$^(language^)=*!
 )
 
-echo "1"
 :: read additional files (assets etc.) from package.manifest
 if exist package.manifest (
   for /F "tokens=*" %%i in (package.manifest) do (
-    set files=%files% %%~nxi
+    set files=!files! %%~nxi
   )
 )
 
 :: copy everything to assembly folder
-robocopy . .package\!name! !files! /S /XD .* /NJH /NJS /NFL /NDL > nul
+robocopy . .package\%name% %files% /S /XD .* /NJH /NJS /NFL /NDL > nul
 
-echo "2"
+
 :: zip it
 pushd .package 
-for  %%A in (!EXCLUDE_FROM_PACK!) do (
-	if exist "%%A"
-	del /S "%%A"
+if not "%DELETE_CUSTOM_FILENAME%" == "" (
+	del /S "%DELETE_CUSTOM_FILENAME%"
 )
+REM delete dev setup
+del /S "*.cmd"
 
-echo "3"
-!zip! a -tzip -bd ..\!archive! !name! > nul
+
+%zip% a -tzip -bd ..\%archive% %name% > nul
 popd
 
 rd /S /Q .package
 
-IF NOT !TARGET_DIRECTORY! == "" (
-	echo * Moving !archive! to !TARGET_DIRECTORY!...
-	move /Y !archive! !TARGET_DIRECTORY!
+IF NOT %TARGET_DIRECTORY% == "" (
+	echo * Moving %archive% to %TARGET_DIRECTORY%...
+	move /Y %archive% %TARGET_DIRECTORY%
 )
 
 :: open it in default browser
-IF NOT !ESOUI_URL! == "" rundll32 url.dll,FileProtocolHandler !ESOUI_URL!
+IF NOT %ESOUI_URL% == "" rundll32 url.dll,FileProtocolHandler %ESOUI_URL%
 
 echo * Done^^!
 echo.
