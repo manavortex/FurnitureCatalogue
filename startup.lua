d095e9a5ff9fa1fa83aa6516028e12cb3baa9f91
Beryl Boozier <beatriceberylbones@gmail.com>
2021-12-28 18:01:07 -0500

    4.312

--- startup.lua
+++ startup.lua
@@ -1,7 +1,7 @@
 FurnitureCatalogue                = {}
 FurnitureCatalogue.name           = "FurnitureCatalogue"
 FurnitureCatalogue.author         = "manavortex"
-FurnitureCatalogue.version        = 4.31
+FurnitureCatalogue.version        = 4.312
 FurnitureCatalogue.CharacterName  = nil
 FurnitureCatalogue.settings       = {}
 
