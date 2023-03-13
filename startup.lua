8aac88d2d57953b99726066090019b551824cfa3
BerylBones <beatriceberylbones@gmail.com>
2023-03-13 10:03:23 -0500

    4.71

--- startup.lua
+++ startup.lua
@@ -3,7 +3,7 @@ FurnitureCatalogue.name           = "FurnitureCatalogue"
 FurnitureCatalogue.author         = "manavortex"
 FurnitureCatalogue.tag            = "FurC"
 
-FurnitureCatalogue.version        = 4.7
+FurnitureCatalogue.version        = 4.71
 FurnitureCatalogue.CharacterName  = nil
 FurnitureCatalogue.settings       = {}
 
