ac2e6e7919abccc2166882e0caedd955b7ad693b
Mat <m.falkowski@dotmat.de>
2024-01-09 23:32:58 +0100

    Confirm Gold Coast scrying item

--- data/Rumours.lua
+++ data/Rumours.lua
@@ -72,7 +72,6 @@ FurC.Rumours[ver.ENDLESS] = {
   [198647] = rumourSource, -- Druidic Drying Rack, Tall",
   [198646] = rumourSource, -- Druidic Drying Rack, Wide",
   [198574] = rumourSource, -- Khajiiti Water Vessel, Large",
-  [198571] = rumourSource, -- Shrine to Dibella",
 }
 
 -- 27 Base Game Patch
