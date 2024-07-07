a37418d12c6b9e5ff9dcc1074939d025f14db900
Mat <m.falkowski@dotmat.de>
2024-07-07 02:24:42 +0200

    lux w27

--- data/Rumours.lua
+++ data/Rumours.lua
@@ -58,7 +58,6 @@ FurC.Rumours[ver.SCIONS] = {
   [203589] = rumourSource, -- Festering Coral, Large Crimson-Orange
   [203588] = rumourSource, -- Alinor Boat, Unfinished
   [203587] = rumourSource, -- Redguard Colonnade, Mosaic
-  [203586] = rumourSource, -- Redguard Archway, Brass
   [203276] = rumourSource, -- Rough Dresser
   [203275] = rumourSource, -- Fish, Silver Trout
   [203274] = rumourSource, -- Box of Tomatoes
